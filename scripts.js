47a1c831aab361ffbad75d75a2d5e5511ad16819
Marvin Botchway <marvinbotchway@outlook.com>
2023-10-01 11:14:37 +0000

    Add Keypad And Styling.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 const solutionText = document.getElementById("solution-text");
 const inputText = document.getElementById("input-text");
+const keypad = document.getElementById("keypad");
+
 
 const validDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const validOperators = ["+", "-", "*", "/"];
+const btnTextContents = [
+    "AC", "+/-", "%", "/",
+    "7", "8", "9", "*",
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    ".", "0", "Del", "="];
 
 let num1Str = "0";
 let num2Str = "0";
@@ -11,6 +19,26 @@ let operator = "";
 let solution;
 
 inputText.addEventListener("input", getInput);
+
+for (let i = 0; i < btnTextContents.length; i++) {
+    const keypadItem = document.createElement("button");
+    
+    keypadItem.classList.add("btn");
+    if (btnTextContents[i] == "=") {
+        keypadItem.classList.add("equal");
+    }
+    else if (btnTextContents[i] == "Del" ||
+    btnTextContents[i] == "AC") {
+        keypadItem.classList.add("del");
+    }
+    else if (!parseInt(btnTextContents[i]) &&
+    btnTextContents[i] != "0" &&
+    btnTextContents[i] != ".") {
+        keypadItem.classList.add("notNum");
+    }
+    keypadItem.textContent = btnTextContents[i];
+    keypad.appendChild(keypadItem).a;
+}
 
 function getInput(e) {
     let valueEntered = e.target.value;
